0429332f69ef19a878a6c217281b338cd8342c48
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-03-27 18:32:39 -0500

    adding login button and social login

--- app/src/main/java/com/example/composegramapp/LoginScreen.kt
+++ app/src/main/java/com/example/composegramapp/LoginScreen.kt
@@ -2,11 +2,10 @@ package com.example.composegramapp
 
 import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
@@ -50,30 +49,49 @@ fun Header(modifier: Modifier) {
 fun Body(modifier: Modifier) {
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var isLoginEnable by rememberSaveable { mutableStateOf(false) }
     Column(modifier = modifier) {
-        ImageLogo()
+        ImageLogo(Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.size(16.dp))
         Email(email) { email = it }
         Spacer(modifier = Modifier.size(4.dp))
         Password(password) { password = it }
         Spacer(modifier = Modifier.size(8.dp))
         ForgotPassword(Modifier.align(Alignment.End))
+        Spacer(modifier = Modifier.size(8.dp))
+        LoginButton(isLoginEnable)
+        Spacer(modifier = Modifier.size(16.dp))
+        LoginDivider()
+        Spacer(modifier = Modifier.size(16.dp))
+        SocialLogin()
     }
 }
 
 @Composable
-fun ImageLogo() {
-    Image(painter = painterResource(id = R.drawable.insta), contentDescription = "logo instagram")
+fun ImageLogo(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.insta),
+        contentDescription = "logo instagram",
+        modifier = modifier
+    )
 }
 
 @Composable
 fun Email(email: String, onTextChanged: (String) -> Unit) {
-    TextField(value = email, onValueChange = { onTextChanged(it) })
+    TextField(
+        value = email,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
 @Composable
 fun Password(password: String, onTextChanged: (String) -> Unit) {
-    TextField(value = password, onValueChange = { onTextChanged(it) })
+    TextField(
+        value = password,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
 @Composable
@@ -85,4 +103,58 @@ fun ForgotPassword(modifier: Modifier) {
         color = Color(0xFF4EA8E9),
         modifier = modifier
     )
+}
+
+@Composable
+fun LoginButton(loginEnable: Boolean) {
+    Button(onClick = { }, enabled = loginEnable, modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Log in")
+    }
+}
+
+@Composable
+fun LoginDivider() {
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+        Text(
+            text = "OR",
+            modifier = Modifier.padding(horizontal = 12.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFB5B5B5)
+        )
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+    }
+}
+
+@Composable
+fun SocialLogin() {
+    Row(
+        Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fb),
+            contentDescription = "social login facebook",
+            modifier = Modifier.size(16.dp)
+        )
+        Text(
+            text = "Continue as Ivan",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9),
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
 }
