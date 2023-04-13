aa9c216ef8be3d7e662be98c4a1d71c470dcbe2d
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-04-13 15:30:19 -0500

    inject LoginViewModel.kt and MainActivity.kt

--- app/src/main/java/com/example/composegramapp/MainActivity.kt
+++ app/src/main/java/com/example/composegramapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.composegramapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,8 +11,12 @@ import androidx.compose.ui.Modifier
 import com.example.composegramapp.login.ui.LoginScreen
 import com.example.composegramapp.login.ui.LoginViewModel
 import com.example.composegramapp.ui.theme.ComposeGramAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LoginScreen(LoginViewModel())
+                    LoginScreen(loginViewModel)
                 }
             }
         }
